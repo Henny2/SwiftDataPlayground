d60c3f3ea3bdf3f749f45390b15750aedac26d5c
Henny2 <henrieke.baunack@web.de>
2023-12-12 19:31:19 -0800

    Initial Commit

---
+++ SwiftDataPlayground/SwiftDataPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataPlaygroundApp.swift
+//  SwiftDataPlayground
+//
+//  Created by Henrieke Baunack on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
