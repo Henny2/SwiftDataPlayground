f0b67d54c99ea17c506231d341703aeac17b11d6
Henny2 <henrieke.baunack@web.de>
2023-12-12 19:51:22 -0800

    moving content to new file

--- SwiftDataPlayground/ContentView.swift
+++ SwiftDataPlayground/ContentView.swift
@@ -7,25 +7,9 @@
 
 import SwiftUI
 
-struct CustomButton: View {
-    @Binding var boolean: Bool
-    var body: some View {
-        Button("Click here"){
-            print("clicked the button: \(boolean)")
-            boolean.toggle()
-        }
-        .frame(width:200, height:200)
-        .background(boolean ? .green : .red)
-    }
-}
-
 struct ContentView: View {
-    @State private var boolean = true
     var body: some View {
-        VStack{
-            CustomButton(boolean: $boolean)
-            Text("the bool value: \(String(boolean))")
-        }
+        Text("Hello")
     }
 }
 
